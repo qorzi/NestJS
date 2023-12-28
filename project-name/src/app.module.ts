60645583892529cc7db562d1907fcce219f6e3f0
qorzi00 <qorzi00@gmail.com>
2023-12-28 14:29:02 +0900

    동적 config 생성 추가

--- project-name/src/app.module.ts
+++ project-name/src/app.module.ts
@@ -4,8 +4,21 @@ import { RedirectController } from './redirect/redirect.controller';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
+
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    EmailModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema
+    })
+  ],
   controllers: [
     ApiController,
     RedirectController
